36dd2a4e32646f98fce8d19756acfaf0dd81999c
jawwad-ali <alijawwad001@gmail.com>
2021-08-23 15:03:40 +0500

    remove unused code

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,6 @@ module.exports = {
       options: {
         fonts: [
           'Yantramanav',
-          // `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
         ],
         display: 'swap'
       }
